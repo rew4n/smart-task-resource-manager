7d951c165f5ad043564fcec031b0cc4d1960ca76
Rewan <rahmanrewan@gmail.com>
2025-12-06 16:35:57 +0000

    Add templates folder and HTML pages (home & tasks)

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Smart Task & Resource Manager is running 🚀"
+    return render_template("home.html")
+
+@app.route("/tasks")
+def tasks():
+    # temporary fake tasks
+    task_list = [
+        {"id": 1, "title": "Buy groceries", "status": "pending"},
+        {"id": 2, "title": "Finish Flask project", "status": "in progress"},
+        {"id": 3, "title": "Clean the house", "status": "completed"}
+    ]
+    return render_template("tasks.html", tasks=task_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
